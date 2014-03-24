fce7b41f660c956411c55de8af7897d23c8de914
Tim Mattison <tim.mattison@controlgroup.com>
2014-03-24 19:20:55 -0400

    Switched back to ping to make things easy

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@
 #PROGRAM_TO_SJMMIE="curl localhost"
 #PROGRAM_TO_SJMMIE="wget localhost -O /dev/null"
 #PROGRAM_TO_SJMMIE="wget localhost"
-PROGRAM_TO_SJMMIE="/Applications/Firefox.app/Contents/MacOS/firefox"
+#PROGRAM_TO_SJMMIE="/Applications/Firefox.app/Contents/MacOS/firefox"
 #PROGRAM_TO_SJMMIE="ls"
 
 #PROGRAM_TO_SJMMIE="/Applications/VLC.app/Contents/MacOS/VLC -I dummy --loop /Users/timmattison/Desktop/annex/github/cg-snippets/python/rccl/horizontal-line.avi --sout '#duplicate{dst=rtp{mux=ts,dst=239.192.128.105,port=1234,sdp=sap://,name=\"TestStream\",ttl=12}}'"
-#PROGRAM_TO_SJMMIE="ping google.com"
+PROGRAM_TO_SJMMIE="ping google.com"
 #PROGRAM_TO_SJMMIE="nslookup google.com"
 
 echo "Without SJMMIE"
