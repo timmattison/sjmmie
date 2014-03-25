e51c75090170e9b996430457e495fbeb81e4b3d3
Tim Mattison <tim.mattison@controlgroup.com>
2014-03-25 14:38:25 -0400

    Removed stray extern

--- sjmmie.c
+++ sjmmie.c
@@ -219,7 +219,7 @@ char* java_byte_array_to_char_array(JNIEnv *env, jbyteArray java_byte_array) {
 }
 
 // For converting char/byte arrays to Java arrays
-extern jbyteArray char_array_to_java_byte_array(JNIEnv *env, char* c_buffer, int c_buffer_length) {
+jbyteArray char_array_to_java_byte_array(JNIEnv *env, char* c_buffer, int c_buffer_length) {
 	if(c_buffer == NULL) {
 		return NULL;
 	}
