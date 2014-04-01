623414ae7c1e2219662063685cd76c8a397316ef
Tim Mattison <tim.mattison@controlgroup.com>
2014-04-01 10:32:03 -0400

    Working on copy back from char array

--- functions/recv.c
+++ functions/recv.c
@@ -39,19 +39,17 @@ ssize_t SJMMIE_recv(int socket, void *buffer, size_t size, int flags) {
 		// NOTE: Since this is recv we technically don't need to copy the data from C.  But to make sure that we reproduce normal
 		//         behavior and bugs as well we will do it.  Otherwise we could mask issues where the caller was reading past the
 		//         end of their original array into data that was put there by C.
-		jbyteArray data_to_recv_java = char_array_to_java_byte_array(env, (char *) buffer, size);
+        C_JAVA_CHAR_ARRAY(buffer, size);
 
 		jint return_value;
 
-		return_value = (*env)->CallIntMethod(env, sjmmie_instance, java_recv_method, socket, data_to_recv_java, size, flags);
+		return_value = (*env)->CallIntMethod(env, sjmmie_instance, java_recv_method, socket, CHAR_ARRAY_UNROLL(buffer), size, flags);
 
 		// Copy the data back from Java to C and release the Java copy immediately
-		char* temp_buffer = java_byte_array_to_char_array(env, data_to_recv_java);
-		memcpy(buffer, temp_buffer, size);
-		safe_release_byte_array_elements(env, data_to_recv_java, (signed char *) temp_buffer);
+        JAVA_C_CHAR_ARRAY_COPY_BACK(buffer, size);
 
 		// Release the copy of the original C data
-		safe_delete_local_ref(env, data_to_recv_java);
+        RELEASE_JAVA_CHAR_ARRAY(buffer);
 	
 		return return_value;
 	}
---
+++ src/main/java/com/timmattison/sjmmie/restlets/BasicRestletApplicationFactory.java
@@ -0,0 +1,7 @@
+package com.timmattison.sjmmie.restlets;
+
+/**
+ * Created by timmattison on 4/1/14.
+ */
+public class BasicRestletApplicationFactory {
+}
