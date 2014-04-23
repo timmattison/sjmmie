86236f7740a5a81a2680a9edce99f49c370fb653
Tim Mattison <tim.mattison@controlgroup.com>
2014-04-23 07:28:46 -0400

    Comments

--- objects/sockaddr.c
+++ objects/sockaddr.c
@@ -1,6 +1,8 @@
 #include "sjmmie.h"
 
 jobject sockaddr_to_reference_sockaddr(JNIEnv *env, struct sockaddr *address, int address_length) {
+    // Address length is the length of the whole address structure including sa_len and sa_family.
+    // We must subtract out the length of these fields to get the length of the sa_data field.
     address_length -= sizeof(address->sa_len);
     address_length -= sizeof(address->sa_family);
 
@@ -40,6 +42,8 @@ struct sockaddr *reference_sockaddr_to_sockaddr(JNIEnv *env, jobject reference_s
     char *sa_data_dest = out->sa_data;
     memcpy(sa_data_dest, sa_data, *address_length);
 
+    // Address length needs to be the length of the whole address structure including sa_len and sa_family.
+    // We must add back the length of these fields to get the length of the entire structure.
     *address_length += sizeof(out->sa_len);
     *address_length += sizeof(out->sa_family);
 
