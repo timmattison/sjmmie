55368d83a905630868d096d8373dde811ed4fd3d
Tim Mattison <tim.mattison@controlgroup.com>
2014-04-30 07:06:11 -0400

    Added logger to BasicRestletApplicationFactory

--- src/main/java/com/timmattison/sjmmie/restlets/BasicRestletApplicationFactory.java
+++ src/main/java/com/timmattison/sjmmie/restlets/BasicRestletApplicationFactory.java
@@ -3,13 +3,21 @@ package com.timmattison.sjmmie.restlets;
 import com.google.inject.assistedinject.Assisted;
 import com.timmattison.sjmmie.controls.Toggle;
 
+import javax.inject.Inject;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by timmattison on 4/1/14.
  */
 public class BasicRestletApplicationFactory implements RestletApplicationFactory {
+    private final Logger logger;
     private RestletApplication restletApplication;
+
+    @Inject
+    public BasicRestletApplicationFactory(Logger logger) {
+        this.logger = logger;
+    }
 
     @Override
     public RestletApplication create(@Assisted("toggles") List<Toggle> toggles) {
