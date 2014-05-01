0cf1027204e5232473bf18943d30727f1a6b9bf6
Tim Mattison <tim.mattison@controlgroup.com>
2014-05-01 18:38:54 -0400

    Revert "Disabled just send, fails"
    This reverts commit 67047883f46e238d4733acb44ea1cbedb2a082a4.

--- src/main/java/com/timmattison/sjmmie/Sjmmie.java
+++ src/main/java/com/timmattison/sjmmie/Sjmmie.java
@@ -58,7 +58,7 @@ public class Sjmmie extends AbstractSjmmie {
         this.sendToInterceptorToggle = sendToInterceptorToggle;
         this.recvFromInterceptor = recvFromInterceptor;
         this.socketInterceptor = socketInterceptor;
-        this.sendInterceptor = null;
+        this.sendInterceptor = sendInterceptor;
         this.recvInterceptor = recvInterceptor;
 
         logger.info("Interceptors wired up");
